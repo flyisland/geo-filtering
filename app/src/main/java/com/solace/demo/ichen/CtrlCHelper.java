37ed65783c8f667cfc16c75d8ac90f512184d68c
flyisland <flyisland@gmail.com>
2022-12-14 16:22:16 +0800

    init commit

---
+++ app/src/main/java/com/solace/demo/ichen/CtrlCHelper.java
@@ -0,0 +1,21 @@
+package com.solace.demo.ichen;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CtrlCHelper {
+    static CountDownLatch doneSignal = new CountDownLatch(1);
+    public static void waitForCtrlC(String prompt) {
+        System.out.println(prompt);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            /** This handler will be called on Control-C pressed */
+            @Override
+            public void run() {
+                doneSignal.countDown();
+            }
+        });
+        try {
+            doneSignal.await();
+        } catch (InterruptedException e) {
+        }
+    }
+}
---
+++ app/src/main/java/com/solace/demo/ichen/MessageListener.java
@@ -0,0 +1,23 @@
+package com.solace.demo.ichen;
+
+import com.solacesystems.jcsmp.BytesXMLMessage;
+import com.solacesystems.jcsmp.JCSMPException;
+import com.solacesystems.jcsmp.XMLMessageListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageListener implements XMLMessageListener {
+    final Logger logger = LoggerFactory.getLogger(MessageListener.class);
+    @Override
+    public void onReceive(BytesXMLMessage bytesXMLMessage) {
+        logger.info("Received Message: type->{}, AttachmentContentLength->{}, ContentLength()->{}",
+                bytesXMLMessage.getClass().getCanonicalName(),
+                bytesXMLMessage.getAttachmentContentLength(),
+                bytesXMLMessage.getContentLength());
+    }
+
+    @Override
+    public void onException(JCSMPException e) {
+        logger.error("Consumer received exception: {}", e.toString());
+    }
+}
