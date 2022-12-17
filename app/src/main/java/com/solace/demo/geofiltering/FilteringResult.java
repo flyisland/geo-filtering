589b3f879bae912259a66f8a1ada9c340d08c52b
flyisland <flyisland@gmail.com>
2022-12-17 14:04:18 +0800

    test: add FilteringResult

--- app/src/main/java/com/solace/demo/geofiltering/FilteringResult.java
+++ app/src/main/java/com/solace/demo/geofiltering/FilteringResult.java
@@ -1,13 +1,34 @@
 package com.solace.demo.geofiltering;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 
 public class FilteringResult {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     double accuracy;
     List<Range> ranges;
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public List<Range> getRanges() {
+        return ranges;
+    }
 
     public FilteringResult(double accuracy, List<Range> ranges) {
         this.accuracy = accuracy;
         this.ranges = ranges;
+    }
+
+    public String toJsonString() {
+        try {
+            var json = objectMapper.writeValueAsString(this);
+            return json;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
---
+++ app/src/test/java/com/solace/demo/geofiltering/FilteringResultTest.java
@@ -0,0 +1,20 @@
+package com.solace.demo.geofiltering;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.Test;
+
+class FilteringResultTest {
+    Path workingDir = Path.of("", "src/test/java", "com/solace/demo/geofiltering");
+
+    @Test
+    void toJsonString() throws Exception {
+        Path file = workingDir.resolve("filtering01.json");
+        String jsonString = Files.readString(file);
+        var request = FilteringRequest.from(jsonString);
+        var result = new FilteringResult(100, Range.genesis(request.polygons));
+        System.out.println(result.toJsonString());
+        assertEquals(1, result.ranges.size());
+    }
+}
